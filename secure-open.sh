95dab667533940d0744ae5165a58f0b0d12c5de7
lapwat <q.lapointe@gmail.com>
2018-05-16 15:20:53 +0200

    comments in secure-open.sh script

--- secure-open.sh
+++ secure-open.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+usage() {
+  echo -e "\nUsage: ./secure_open.sh [-w] path/to/file_or_directory\n" 1>&2
+  exit 1
+} 
+
+# check that an argument was given
 if [[ $# -eq 0 ]]; then
   echo "You must specify a file or directory to open."
-  exit 0
+  usage
 fi
 
-for LASTARG; do true; done
-if [[ ! -e "$LASTARG" ]]; then
-  echo "$LASTARG does not exist."
-  exit 0
-fi
+for RELATIVE_PATH; do true; done
+NAME=$(basename "$RELATIVE_PATH") || exit 1
+DIR=$(cd "$(dirname "$RELATIVE_PATH")"; pwd) || exit 1
+ABSOLUTE_PATH="$DIR/$NAME"
 
+# check if read-write option was given
 RW=false
 while getopts "w" OPT
 do
@@ -18,17 +24,22 @@ do
     w)
       RW=true
       ;;
+    \?)
+      usage
+      ;;
   esac
 done
-
-NAME=$(basename "$LASTARG")
-DIR="$(cd "$(dirname "$LASTARG")"; pwd)"
 if [[ "$RW" = true ]]; then
   READOPTION="rw"
 else
   READOPTION="ro"
 fi
-VOLUME_VALUE="$DIR"/"$NAME":/app/data/"$NAME":"$READOPTION"
+
+# check that the path exists
+if [[ ! -e "$ABSOLUTE_PATH" ]]; then
+  echo "$ABSOLUTE_PATH does not exist."
+  usage
+fi
 
 xhost +local:docker
-VOLUME_VALUE="$VOLUME_VALUE" docker-compose up -d
+ABSOLUTE_PATH="$ABSOLUTE_PATH" NAME="$NAME" READOPTION="$READOPTION" docker-compose up -d
